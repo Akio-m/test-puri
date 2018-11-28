b02c3d2fec49fe526fcc1096afd0adc23d11ee23
akio-m <k168577@ie.u-ryukyu.ac.jp>
2018-11-28 15:49:20 +0000

    init app.

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := """purichan-map"""
+organization := "com.sample"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.12.6"
+
+libraryDependencies += guice
---
+++ target/scala-2.12/twirl/main/views/html/index.template.scala
@@ -0,0 +1,61 @@
+
+package views.html
+
+import _root_.play.twirl.api.TwirlFeatureImports._
+import _root_.play.twirl.api.TwirlHelperImports._
+import _root_.play.twirl.api.Html
+import _root_.play.twirl.api.JavaScript
+import _root_.play.twirl.api.Txt
+import _root_.play.twirl.api.Xml
+import models._
+import controllers._
+import play.api.i18n._
+import views.html._
+import play.api.templates.PlayMagic._
+import java.lang._
+import java.util._
+import scala.collection.JavaConverters._
+import play.core.j.PlayMagicForJava._
+import play.mvc._
+import play.api.data.Field
+import play.mvc.Http.Context.Implicit._
+import play.data._
+import play.core.j.PlayFormsMagicForJava._
+
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+
+  /**/
+  def apply/*1.2*/():play.twirl.api.HtmlFormat.Appendable = {
+    _display_ {
+      {
+
+
+Seq[Any](format.raw/*2.1*/("""
+"""),_display_(/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+  """),format.raw/*4.3*/("""<h1>Welcome to Play!</h1>
+  <h2>I'm here.</h2>
+""")))}),format.raw/*6.2*/("""
+"""))
+      }
+    }
+  }
+
+  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+
+  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+
+  def ref: this.type = this
+
+}
+
+
+              /*
+                  -- GENERATED --
+                  DATE: Tue Oct 23 15:26:17 UTC 2018
+                  SOURCE: /home/vagrant/purichan-map/app/views/index.scala.html
+                  HASH: 38aae81fc74351e7f705ab0e1a9378e8a9644782
+                  MATRIX: 941->1|1037->4|1064->6|1095->29|1134->31|1163->34|1240->82
+                  LINES: 28->1|33->2|34->3|34->3|34->3|35->4|37->6
+                  -- GENERATED --
+              */
+          
